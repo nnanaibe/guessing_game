4671fca5815e27061e7a364328c436c4ec88bf8b
nnana ibe george <nnanaibe@gmail.com>
2022-11-28 15:39:20 +0100

    final touches

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 extern crate rand;
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-use rand::Rng;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guessing game");
-    
-   loop {
-    println!("Please input guess:");
-let mut guess = String::new();
-io::stdin()
-.read_line(&mut guess)
-.expect("Error reading line.");
-let guess:u32 = guess.trim().parse();
-match guess {
-    Ok(num) => num,
-    Err(_) => continue
-}
 
-println!("Your guess is: {} ",guess);
-match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big"),
-    Ordering::Equal =>  {
-        println!("You win!");
-        break;
+    loop {
+        println!("Please input guess:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+        //trimming our guess which is a string, to remove whitespaces that might or definitely do
+        //exist and then call the parse function on the resulting string to convert it into an integer
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guess is: {} ", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-}
-   } 
 }
