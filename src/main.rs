c41ea22ec51a375b29ea3a48e696829ebd542c1c
nnana ibe george <nnanaibe@gmail.com>
2022-11-28 14:45:41 +0100

    i trimmed the guess which originally is a string, and then introduced a loop block to enable user to keep guessing until user makes the correct guess.

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guessing game");
     
+   loop {
     println!("Please input guess:");
 let mut guess = String::new();
 io::stdin()
 .read_line(&mut guess)
 .expect("Error reading line.");
-let guess:u32 = guess.trim().parse().expect("Please type a number!");
+let guess:u32 = guess.trim().parse();
+match guess {
+    Ok(num) => num,
+    Err(_) => continue
+}
 
 println!("Your guess is: {} ",guess);
 match guess.cmp(&secret_number) {
     Ordering::Less => println!("Too small!"),
     Ordering::Greater => println!("Too big"),
-    Ordering::Equal => println!("You win!"),
+    Ordering::Equal =>  {
+        println!("You win!");
+        break;
+    }
 }
+   } 
 }
