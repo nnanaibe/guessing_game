c3e031414a69af2d99b6c74a1eb53beb7c11580d
nnana ibe george <nnanaibe@gmail.com>
2022-11-28 01:36:34 +0100

    trimmed the user's guess input and converted it to a number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 fn main() {
@@ -10,6 +11,12 @@ let mut guess = String::new();
 io::stdin()
 .read_line(&mut guess)
 .expect("Error reading line.");
+let guess:u32 = guess.trim().parse().expect("Please type a number!");
 
 println!("Your guess is: {} ",guess);
+match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),
+    Ordering::Greater => println!("Too big"),
+    Ordering::Equal => println!("You win!"),
+}
 }
